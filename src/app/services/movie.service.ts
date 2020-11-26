d192ca5e7d2adf5cd08e3ca0c3c3a46158822435
vasilijus <vasiliokas@gmail.com>
2020-11-26 00:08:32 +0000

    movie services/ page

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -2,27 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+ 
+// Typescript custom enum for search types (optional)
 export enum SearchType {
-  all     = '',
-  movie   = 'movie',
-  series  = 'series',
+  all = '',
+  movie = 'movie',
+  series = 'series',
   episode = 'episode'
 }
-
-
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-  url     = "http://www.omdbapi.com";
-  apiKey  = "5da9e9ba";
-
+  url = 'http://www.omdbapi.com/';
+  apiKey = ''; // <-- Enter your own key here!
+ 
   /**
    * Constructor of the Service with Dependency Injection
    * @param http The standard Angular HttpClient to make requests
    */
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient) { }
+ 
   /**
   * Get data from the OmdbApi 
   * map the result to return only the results that we need
@@ -31,13 +32,13 @@ export class MovieService {
   * @param {SearchType} type movie, series, episode or empty
   * @returns Observable with the search results
   */
-  searchData( title: string, type: SearchType ): Observable<any> {
-    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${this}&apikey=${this.apiKey}`).pipe(
+  searchData(title: string, type: SearchType): Observable<any> {
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`).pipe(
       map(results => results['Search'])
     );
   }
-
-   /**
+ 
+  /**
   * Get the detailed information for an ID using the "i" parameter
   * 
   * @param {string} id imdbID to retrieve information
@@ -46,5 +47,4 @@ export class MovieService {
   getDetails(id) {
     return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
   }
-
 }
